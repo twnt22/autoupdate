a2df15a19592a9d727269ab5548ead04f31dd4d9
twnt22 <twnt22@gmail.com>
2016-04-14 16:35:17 +0200

    Revert "/web/upload/"
    This reverts commit bdf90031d99626547dc18310d23802fcdf0c2ec2.

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$xml=simplexml_load_file("lastest.xml") or die("Error: Cannot create object");
-print_r($xml);
-?>
---
+++ web/update/index.php
@@ -0,0 +1,4 @@
+<?php
+$xml=simplexml_load_file("lastest.xml") or die("Error: Cannot create object");
+print_r($xml);
+?>
